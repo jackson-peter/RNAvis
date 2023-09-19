cb8fe19c1e0872e2e9900f8326ee975dd08591e7
jackson-peter <jackson.peter90@gmail.com>
2023-09-19 16:35:15 +0200

    simplifying ui codes

--- app.R
+++ app.R
@@ -1,12 +1,12 @@
 source("global.R")
 
-
 #######################################################################################################################
 #######################################################################################################################
 ############################################       FUNCTIONS       ####################################################
 #######################################################################################################################
 #######################################################################################################################
 
+# Sort file and tabix
 sort_and_tabix <- function(tail_f) {
   bgzip <- file.path(HTSLIB_PATH,"bgzip")
   tabix <- file.path(HTSLIB_PATH,"tabix")
@@ -20,9 +20,10 @@ sort_and_tabix <- function(tail_f) {
   system(tabix_cmd)
 }
 
+# Check if tabixed and tabix if not
 check_if_tabixed <- function(tabix_file,tail_file, index=".tbi") {
   if (!file.exists(tail_file)) {
-    shinyalert("FFFFFFF", paste("THERE IS NO", tail_file), type = "error")
+    shinyalert("OUCH", paste("THERE IS NO", tail_file), type = "error")
   }
 
   if (!file.exists(tabix_file) | !file.exists(paste0(tabix_file, index))){
@@ -31,79 +32,81 @@ check_if_tabixed <- function(tabix_file,tail_file, index=".tbi") {
     sort_and_tabix(tail_file)
     remove_modal_spinner() # remove it when done
 
-    
   } else {
     shinyalert("Nice!", paste("successfully added", tabix_file), type = "success")
   }
 }
 
+# Read only part of files containing users' AGI 
 read_tabixed_files <- function(file, AGI) {
-  dt <- fread(cmd = paste(file.path(HTSLIB_PATH, "tabix"), file, AGI, "-h"))
+  dt <- fread(cmd = paste(file.path(HTSLIB_PATH, "tabix"), file, AGI, "-h")) 
   return(dt)
 }
-  
 
 #######################################################################################################################
 #######################################################################################################################
 ############################################       UI       ###########################################################
 #######################################################################################################################
 #######################################################################################################################
-ui <- dashboardPage(
-  dashboardHeader(title = "RNA visualization",
-                  dropdownMenuOutput("notificationMenu")), #dashboardHeader
-  
-  dashboardSidebar(
-    sidebarMenu( 
-      br(),
-      h4(HTML("&nbsp; 1 | Select a FLEPseq run folder")),
-      shinyDirButton("dir", 'Select a folder', 'Please select a run folder  (use the arrows)'),
-      
+header= dashboardHeader(title = "RNAvis", dropdownMenuOutput("notificationMenu"))
 
-      br(),
-      
-      h4(HTML("&nbsp; 2 | Enter an AGI to search for")),
-      textInput("AGI",value = "AT1G01010.1",label = NULL ), #textInput
-      
-      # Select variables to display ----
-      uiOutput("checkbox"),
-      actionButton(inputId = "SubmitAGI",
-                   label = "Submit AGI NOW",
-                   style="color: #fffc300; background-color: #e95420; border-color: #c34113;
+sidebar <-   dashboardSidebar(
+  sidebarMenu( 
+    br(),
+    h4(HTML("1 | Select a FLEPseq folder")),
+    shinyDirButton("dir", 'Select a folder', 'Please select a run folder  (use the arrows)'),
+    br(),
+    h4(HTML("2 | Enter an AGI")),
+    textInput("AGI",value = "AT1G01010.1",label = NULL ), #textInput
+    actionButton(inputId = "SubmitAGI",
+                 label = "Get table",
+                 style="color: #fffc300; background-color: #e95420; border-color: #c34113;
                                                          >border-radius: 10px; >border-width: 2px"),
+    
+    h4(HTML("3 | Select usefull columns")),
+    selectizeInput('column_sel','Select column',choices=NULL, selected=NULL, multiple=T),
+    actionButton(inputId = "update",
+                 label = "Update columns"),
+    
+    br(),
+    h4(HTML("4 | Export Table")),
+    checkboxInput("save_selection", "Only save selected columns?", value = FALSE, width = NULL),
+    actionButton("save_df","Export Table")
+    
+  ) #sidebarMenu
+)
 
-      
-      br(),
-     
-      
-      h4(HTML("&nbsp; 5 | Export Table")),
-      
-      actionButton("save_df",
-                   "Export Table")
-    ) #sidebarMenu
-  ), #dashboardSidebar
-  
-  dashboardBody(
-    fluidRow(
-      tabsetPanel(
-        tabPanel("mRNA statistics",
-                 tabsetPanel(
-                   title = NULL,
-                   # The id lets us use input$tabset1 on the server to find the current tab
-                   id = "tabset1",
-                   
-                   tabPanel("DataTable",
-                            dataTableOutput("AGI_dt")
-                            
-                   ),
-                   
-                 ) #tabsetPanel
-        ) #tabPanel
-      ) #tabsetPanel
-    ),
-    fluidRow(infoBoxOutput("tabset1Selected"))
-  )
-) #ui
 
+body <- dashboardBody(
+  tags$head(
+    tags$link(rel = "stylesheet", type = "text/css", href = "custom.css")
+  ),
+  tabsetPanel(
+    tabPanel("General mRNA statistics",
+             h2(textOutput("run_name")),
+               fluidRow(
+                 dataTableOutput("barcode_geno"),
+                 hr(),
+               ),
+               fluidRow(
+                 splitLayout(cellWidths = c("50%", "50%"), plotOutput("coverage"), plotOutput("mapq")),
+                 hr(),
+               ), 
+               
+      ), #tabPanel
+      tabPanel("Specific AGI",
+               h2(textOutput("AGI_name")),
+               fluidRow(dataTableOutput("AGI_dt"))
+      ) # tabPanel
+    ), #tabsetPanel
+
+) # /dashboardbody
+
+ui <- dashboardPage(skin="green",
+  header, 
+  sidebar,
+  body
+)
 
 #######################################################################################################################
 #######################################################################################################################
@@ -111,26 +114,16 @@ ui <- dashboardPage(
 #######################################################################################################################
 #######################################################################################################################
 
-
 server <- function(input, output, session) {
   
-  
   # Fait débuter le browser de fichiers dans le home.
-  shinyDirChoose(
-    input,
-    'dir',
-    roots = c(home = './data')
-  ) #shinyDirChoose
-  
+  shinyDirChoose(input, 'dir', roots = c(home = './data'))
   
   global <- reactiveValues(datapath = getwd())
   dir <- reactive(input$dir)
-  output$dir <- renderText({ 
-    global$datapath 
-  }) #output$dir
+  output$dir <- renderText({global$datapath}) #output$dir
   
-  
-  # Met a jour le path du dossier choisi.
+  # Met a jour le path du dossier choisi, associe les barcodes aux génotypes
   observeEvent(ignoreNULL = TRUE,
                eventExpr = {input$dir},
                handlerExpr = {
@@ -139,56 +132,117 @@ server <- function(input, output, session) {
                  global$datapath <- file.path(home,
                                               paste(unlist(dir()$path[-1]),
                                                     collapse = .Platform$file.sep))
-                 global$tail_files <- list.files(path=file.path(global$datapath,tail_dir), pattern = paste0(extension, "$"),full.names = T)
-                 global$barcode_file <- file.path(global$datapath, sample_table)
-               }) #observeEvent
-  
-
-  # Actualise les checkboxes avec les noms des barcodes présents dans le dossier.
-  observeEvent(
-    input$dir, {
-      
-      #tail_files=list.files(path=file.path(global$datapath,tail_dir), pattern = paste0(extension, "$"),full.names = T)
-      tail_files=global$tail_files
-
-      if (length(tail_files)>0) {
-
-        barcode_corr <- fread(global$barcode_file, col.names = c("barcode", "genotype"), header = F) %>%
-          mutate(tabix_file= file.path(global$datapath, tail_dir, paste0(barcode, ".sorted.csv.gz")),
-                 tail_file=file.path(global$datapath, tail_dir, paste0(barcode, extension)))
-        
+                 tail_files <- list.files(path=file.path(global$datapath,tail_dir), pattern = paste0(tail_ext, "$"),full.names = T)
+                 barcode_file <- file.path(global$datapath, sample_table)
+                 mapping_files <- list.files(path=file.path(global$datapath,mapping_dir), pattern=paste0(mapping_ext, "$"), full.names=T)
+                 
+                 if (length(tail_files)>0) {
+                   global$barcode_corr <- fread(barcode_file, col.names = c("barcode", "genotype"), header = F) %>%
+                     mutate(tabix_file= file.path(global$datapath, tail_dir, paste0(barcode, ".sorted.csv.gz")),
+                            tail_file=file.path(global$datapath, tail_dir, paste0(barcode, tail_ext)),
+                            map_file=file.path(global$datapath, mapping_dir, paste0(barcode, mapping_ext)))
         # Check if files are tabixed, and tabix them if not
-        apply(barcode_corr[,c('tabix_file','tail_file')], 1, function(y) check_if_tabixed(y['tabix_file'],y['tail_file']))
+        apply(global$barcode_corr[,c('tabix_file','tail_file')], 1, function(y) check_if_tabixed(y['tabix_file'],y['tail_file']))
         
-      }
+      } 
     }
   )
   
+  # Mapping data
+  MAP_data <- eventReactive(input$dir,{
+    map_files <- global$barcode_corr$map_file
+    names(map_files) <- global$barcode_corr$genotype
+    mapping_q <- rbindlist(lapply(map_files, fread, col.names=mapping_cols), idcol = "origin")
+  })
+  
   # AGI_data is the data table of the AGI
   AGI_data <- eventReactive(input$SubmitAGI,{
-    print("building DF")
-    barcode_corr <- fread(global$barcode_file, col.names = c("barcode", "genotype"), header = F) %>%
-      mutate(tabix_file= file.path(global$datapath, tail_dir, paste0(barcode, ".sorted.csv.gz")),
-             tail_file=file.path(global$datapath, tail_dir, paste0(barcode, extension)))
-    
-    column_names <- names(fread(cmd = paste('head -n 500', barcode_corr$tail_file[1])))
+    req(input$dir, input$AGI)
+    column_names <- names(fread(cmd = paste('head -n 1', global$barcode_corr$tail_file[1])))
     column_names <- c('origin', column_names)
-    tabixed_list <- barcode_corr$tabix_file
-    names(tabixed_list) <- barcode_corr$genotype
-    
-    
+    tabixed_list <- global$barcode_corr$tabix_file
+    names(tabixed_list) <- global$barcode_corr$genotype
     tabixed_df = setDT(as.list(tabixed_list))
-    
-
     AGI_DF <- rbindlist(lapply(tabixed_df, read_tabixed_files, AGI=input$AGI), idcol = "origin")
     colnames(AGI_DF) <- column_names
+    AGI_DF <- AGI_DF %>% mutate(Run=basename(global$datapath))
     #print(head(AGI_DF))
+    updateSelectizeInput(session, "column_sel", choices=colnames(AGI_DF))
+
     return(AGI_DF)
   })
   
-  output$AGI_dt <- renderDataTable(AGI_data())
+  filtereddata <- eventReactive({
+    input$update
+    AGI_data()
+  },  {
+    req(AGI_data())
+    if (is.null(input$column_sel) || input$column_sel == "") {
+      AGI_data()
+    } else {
+      print(input$column_sel)
+      AGI_data()[ ,colnames(AGI_data()) %in% input$column_sel, with=FALSE]
+    } 
+  })
   
+  observeEvent(AGI_data(), {
+    updateSelectInput(session, "select", choices=colnames(AGI_data()))
+  })
 
+  output$run_name <- renderText({
+    req(AGI_data())
+    title <- unique(AGI_data()$Run)
+  })
+  
+  output$AGI_name <- renderText({
+    req(AGI_data())
+    title <- unique(AGI_data()$mRNA)
+  })
+  
+  output$coverage <- renderPlot({
+    req(AGI_data())
+    cov <- MAP_data() %>%
+      group_by(origin, rname) %>%
+      summarise(mean_cov=mean(coverage),
+                mean_mapq =mean(meanmapq))
+    ggplot(cov, aes(x=rname, y=mean_cov, fill=origin)) +
+      geom_col(position = "dodge") +
+      ggtitle("Mean coverage") +
+      theme(
+        legend.position = c(.05, .95),
+        legend.justification = c("left", "top"),
+        legend.box.just = "left",
+        legend.margin = margin(6, 6, 6, 6),
+        legend.background = element_rect(fill="white",
+                                         size=0.5, linetype="solid", 
+                                         colour ="black")
+      )
+  })
+  
+  output$mapq <- renderPlot({
+    req(AGI_data())
+    cov <- MAP_data() %>%
+      group_by(origin, rname) %>%
+      summarise(mean_cov=mean(coverage),
+                mean_mapq =mean(meanmapq))
+    ggplot(cov, aes(x=rname, y=mean_mapq, fill=origin)) +
+      geom_col(position = "dodge") +
+      ggtitle("Mean mapping quality")+
+      theme(legend.position = "none")
+  })
+
+  output$AGI_dt <- renderDataTable(filtereddata(), options = list(scrollX = TRUE))
+  
+  output$barcode_geno <- renderDataTable(
+    #req(AGI_data())
+    global$barcode_corr,
+    options = list(scrollX = TRUE)
+    )
+  
+  observeEvent(AGI_data(),updateSelectizeInput(session, "column", choices=names(AGI_data())))
+  
+  
+  ######## SAVE TABLE
   # Fait débuter le browser de fichiers de sauvegarde de df dans le home.
   shinyDirChoose(
     input,
@@ -196,14 +250,12 @@ server <- function(input, output, session) {
     roots = c(home = './data')
   ) #shinyDirChoose
   
-  
   save_global <- reactiveValues(datapath = getwd())
   save_dir <- reactive(input$save_dir)
   
   output$save_dir <- renderText({ 
     save_global$datapath 
   }) #output$dir
-  
   
   # Met a jour le path du dossier choisi.
   observeEvent(ignoreNULL = TRUE, eventExpr = {input$save_dir},
@@ -215,13 +267,12 @@ server <- function(input, output, session) {
                                                          collapse = .Platform$file.sep))
                }) #observeEvent
   
-  
-  
   # Fenêtre de confirmation de sauvegarde du dataframe.
   observeEvent(input$save_df, {
+    default_name <- paste0(input$AGI, "tails.tsv")
     showModal(modalDialog(
       tagList(
-        textInput("filename", label = "Filename", placeholder = paste0(input$AGI, "_tails.tsv")),
+        textInput("filename", label = "Filename", placeholder = default_name, value = default_name),
         
         shinyDirButton("save_dir", 'Select where to save the dataframe', 'Please select a folder', FALSE),
         verbatimTextOutput("save_dir", placeholder = TRUE)
@@ -233,26 +284,22 @@ server <- function(input, output, session) {
     ))
   })
   
-  
   # Enregistre le dataframe en .tsv.
   observeEvent(input$confirmCreate, { 
-    
-    # Requiert un nom de fichier pour créer la sauvegarde.
     req(input$filename)
-    
-    # Close the confirmation window.
     removeModal()
     
     show_modal_spinner(spin = "fingerprint", text=paste("exporting table")) # show the modal window
-    write_tsv(AGI_data(), file.path(save_global$datapath, input$filename))
-    shinyalert("Nice!", paste("Export successfull"), type = "success")
+    if (input$save_selection) {
+      write_tsv(filtereddata(), file.path(save_global$datapath, input$filename))
+    } else {
+      write_tsv(AGI_data(), file.path(save_global$datapath, input$filename))
+    }
+    
+    shinyalert("Nice!", paste("Export successfull", file.path(save_global$datapath, input$filename)), type = "success")
     remove_modal_spinner() # remove it when done
   })
   
-  
-  
 } #server
-
-
 
 shinyApp(ui, server)
